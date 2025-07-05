2b4bdca243eb3a3d9ef384f7d78bf36d5256576a
Anant <Thalaanaaa@gmail.com>
2025-07-05 15:25:21 +0530

    Age Screen Complete

--- lib/pages/age_group_page.dart
+++ lib/pages/age_group_page.dart
@@ -8,8 +8,61 @@ class AgeGroupPage extends StatefulWidget {
 }
 
 class _AgeGroupPageState extends State<AgeGroupPage> {
+  String? _selectedAgeGroup;  
+  final List<String> _ageGroups = [
+    '1-10',
+    '11-20',
+    '21-30',
+    '31-40',
+    '41-50',
+    '51-60',
+    '61-70',
+    '71+',
+  ];
   @override
   Widget build(BuildContext context) {
-    return  Container();
+    return  Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Select Age Group',style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.transparent,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Please select your age group:',
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 20),
+            DropdownButton<String>(
+              value: _selectedAgeGroup,
+              hint: const Text('Select Age Group'),
+              items: _ageGroups.map((String ageGroup) {
+                return DropdownMenuItem<String>(
+                  value: ageGroup,
+                  child: Text(ageGroup),
+                );
+              }).toList(),
+              onChanged: (String? newValue) {
+                setState(() {
+                  _selectedAgeGroup = newValue;
+                });
+              },
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                if (_selectedAgeGroup != null) {
+                  Navigator.pop(context, _selectedAgeGroup);
+                }
+              },
+              child: const Text('Submit'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
